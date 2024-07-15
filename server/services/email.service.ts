da047bd18a8f4791d36c86dd2a525273bc309e47
Vaibhav Mishra <vaibhav@karmadhi.com>
2024-07-15 17:23:34 +0530

    mobile number validation

--- server/services/email.service.ts
+++ server/services/email.service.ts
@@ -57,22 +57,22 @@ export default class EmailService {
                             <tr>
                                 <td style="text-align: center;">
                                     <span
-                                        style="margin: 0px;padding-bottom: 25px; text-transform: uppercase;font-size: 30px;font-weight: 500;font-family: system-ui;">Email
+                                        style="margin: 0px;padding-bottom: 25px; text-transform: uppercase;font-size: 30px;font-weight: 500;font-family: system-ui;color:#0F3462;">Email
                                         and
                                         password
                                     </span>
                                     <p
-                                        style=" margin: 0px 32px;padding-bottom: 25px;line-height: 33px; font-size: 18px;padding-top: 20px;font-family: system-ui;">
+                                        style=" margin: 0px 32px;padding-bottom: 25px;line-height: 33px; font-size: 18px;padding-top: 20px;font-family: system-ui;color:#0F3462;">
                                         Please use the following email and password to access the system. If you encounter any
                                         issues during the login process,
                                         feel free to reach out for assistance. Thank you for your cooperation.
 
                                     </p>
                                     <h2
-                                        style="margin: 0px; padding-bottom: 20px;font-family: system-ui;font-weight: 500;font-size: 21px;">
+                                        style="margin: 0px; padding-bottom: 20px;font-family: system-ui;font-weight: 500;font-size: 21px;color:#0F3462;">
                                         Email: <span style="text-decoration: underline;">${to}</span></h2>
                                     <h2
-                                        style="margin: 0px; padding-bottom: 30px;font-family: system-ui;font-weight: 500;font-size: 21px;">
+                                        style="margin: 0px; padding-bottom: 30px;font-family: system-ui;font-weight: 500;font-size: 21px;color:#0F3462;">
                                         Password:
                                         <span>${data.password}</span>
                                     </h2>
